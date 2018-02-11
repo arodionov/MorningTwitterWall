6e1f8e1cd0a3fd5ac243d0911ca84d5d49b5ab32
arodionov <RandreyArtndf93>
2018-02-11 13:27:49 +0200

    Moved to Gradle Kotlin DSL

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    application
+    kotlin("jvm") version "1.2.20"
+}
+
+application {
+    mainClassName = "ua.kug.TwitterWallJSON"
+}
+
+val twitter4jVer = "4.0.6"
+val jacksoCoreVer = "2.9.2"
+
+dependencies {
+    compile(kotlin("stdlib"))
+    compile("org.twitter4j", "twitter4j-core", twitter4jVer)
+    compile("org.twitter4j", "twitter4j-stream", twitter4jVer)
+    compile("io.javalin", "javalin", "1.0.0")
+    compile("com.fasterxml.jackson.core", "jackson-core", jacksoCoreVer)
+    compile("com.fasterxml.jackson.core", "jackson-databind", jacksoCoreVer)
+    compile("org.jetbrains.kotlinx", "kotlinx-coroutines-core", "0.22.1")
+    testCompile("org.jetbrains.kotlin", "kotlin-test-junit", "1.2.21")
+    testCompile("junit", "junit", "4.12")
+    testCompile("io.kotlintest", "kotlintest", "2.0.1")
+    testCompile("com.nhaarman", "mockito-kotlin", "0.9.0")
+
+}
+
+repositories {
+    jcenter()
+}
