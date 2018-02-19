a056a5e8646387c29c810597d164a8a91521180a
arodionov <RandreyArtndf93>
2018-02-19 11:33:14 +0200

    Temporal transition

--- src/main/kotlin/ua/kug/boot/Application.kt
+++ src/main/kotlin/ua/kug/boot/Application.kt
@@ -1,10 +1,50 @@
 package ua.kug.boot
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Scope
+import twitter4j.Status
+import twitter4j.TwitterFactory
+import twitter4j.TwitterStreamFactory
+import twitter4j.util.function.Consumer
+import ua.kug.tw.RetweetAction
+import ua.kug.tw.TwitterWallWithAction
 
 @SpringBootApplication
-class Application
+class Application {
+
+    @Bean
+    fun twitterService(tw: TwitterWallWithAction): TwitterService {
+        return TODO()
+    }
+
+    @Bean
+    @Scope("prototype")
+    fun twitterWall(@Value("#Java, #Kotlin") tags: List<String>, actions: List<Consumer<Status>>) =
+            TwitterWallWithAction(
+                    twitterStream = TwitterStreamFactory().instance,
+                    hashatgs = tags,
+                    size = 5,
+                    actions = actions
+            )
+
+    //@Bean
+    fun retweetAction() =
+            RetweetAction(
+                    twitter = TwitterFactory().instance,
+                    stopWords = listOf("ugly", "no soup", "bad", "yegor256")
+            )
+
+    @Bean
+    fun printAction() =
+            Consumer<Status> {
+                println(it.text)
+                println(Thread.currentThread().name)
+            }
+
+}
 
 fun main(args: Array<String>) {
     SpringApplication.run(Application::class.java, *args)
--- src/main/kotlin/ua/kug/boot/TwitterService.kt
+++ src/main/kotlin/ua/kug/boot/TwitterService.kt
@@ -8,37 +8,14 @@ import ua.kug.tw.RetweetAction
 import ua.kug.tw.TwitterWallWithAction
 import javax.annotation.PostConstruct
 
-@Service
-class TwitterService {
+abstract class TwitterService(var twitterWall: TwitterWallWithAction) {
 
-    private var twitterWall: TwitterWallWithAction? = null
-
-    @PostConstruct
-    fun init() {
-        twitterWall = twitterWall(listOf("#KUG", "#MorningAtLohika", "#Kotlin"))
-    }
-
-    private fun twitterWall(tags: List<String>) =
-            TwitterWallWithAction(
-                    twitterStream = TwitterStreamFactory().instance,
-                    hashatgs = tags,
-                    size = 5,
-                    actions = listOf(
-                            RetweetAction(
-                                    twitter = TwitterFactory().instance,
-                                    stopWords = listOf("ugly", "no soup", "bad", "yegor256")
-                            ),
-                            Consumer {
-                                println(it.text)
-                                println(Thread.currentThread().name)
-                            }
-                    )
-            )
-
-    fun tweets() = twitterWall!!.tweets()
+    fun tweets() = twitterWall.tweets()
 
     fun subscribe(tags: List<String>) {
-        twitterWall = twitterWall(tags)
+        twitterWall = createTwitterWall()
     }
+
+    abstract fun createTwitterWall(): TwitterWallWithAction
 
 }
