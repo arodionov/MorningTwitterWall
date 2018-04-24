79c6a7b3929008fc5922b355d0b8946e02ec90cb
Ihor Banadiga <banadiga@gmail.com>
2018-04-24 16:40:58 +0300

    merge properties

--- src/main/kotlin/ua/kug/TwitterWallJSON.kt
+++ src/main/kotlin/ua/kug/TwitterWallJSON.kt
@@ -3,6 +3,7 @@ package ua.kug
 import io.javalin.Javalin
 import twitter4j.TwitterFactory
 import twitter4j.TwitterStreamFactory
+import twitter4j.conf.ConfigurationBuilder
 import twitter4j.util.function.Consumer
 import ua.kug.tw.RetweetAction
 import ua.kug.tw.TwitterWallWithAction
@@ -19,8 +20,15 @@ fun main(args: Array<String>) {
     val app = Javalin.create().port(Integer.valueOf(prop.getProperty("server.port", "7000"))).start()
     app.get("/") { ctx -> ctx.result("Hello World") }
 
+    val configurationBuilder = ConfigurationBuilder()
+    configurationBuilder.setDebugEnabled(true)
+            .setOAuthConsumerKey(prop.getProperty("oauth.consumerKey"))
+            .setOAuthConsumerSecret(prop.getProperty("oauth.consumerSecret"))
+            .setOAuthAccessToken(prop.getProperty("oauth.accessToken"))
+            .setOAuthAccessTokenSecret(prop.getProperty("oauth.accessTokenSecret"))
+
     val twitterWall = TwitterWallWithAction(
-            twitterStream = TwitterStreamFactory().instance,
+            twitterStream = TwitterStreamFactory(configurationBuilder.build()).instance,
             hashatgs = prop.getProperty("app.hashatgs", "#KUG,#MorningAtLohika,#Kotlin").split(","),
             size = 5,
             actions = listOf(
